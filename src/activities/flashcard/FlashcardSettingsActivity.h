c0f20db1caa400513be978521cf310e0ecbd58ab
vardhan347-ui <vardhan347@gmail.com>
2026-05-04 19:57:53 -0700

    FlashcardSettingsActivity.h

--- src/activities/flashcard/FlashcardSettingsActivity.h
+++ src/activities/flashcard/FlashcardSettingsActivity.h
@@ -9,6 +9,7 @@ class FlashcardSettingsActivity final : public Activity {
 
     uint8_t newPerDay = 10;
     uint8_t maxReview = 250;
+    uint8_t fontSizeIndex = 1;  // 0=Small 1=Medium 2=Large 3=XLarge
 
     void saveSettings();
 
